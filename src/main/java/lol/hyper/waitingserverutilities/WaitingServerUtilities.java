2d12722296250102b8a969cb3455e0e23a2a823d
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2020-04-21 18:52:08 -0400

    Code cleanup

--- src/main/java/lol/hyper/waitingserverutilities/WaitingServerUtilities.java
+++ src/main/java/lol/hyper/waitingserverutilities/WaitingServerUtilities.java
@@ -12,25 +12,25 @@ import org.bukkit.event.entity.EntityCombustByBlockEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+
 public final class WaitingServerUtilities extends JavaPlugin implements Listener {
+
+    public static final HashMap<Player, Long> lastChange = new HashMap<>(); // x1D - Offhand Swap fix
+    public static final HashMap<Player, Integer> warnings = new HashMap<>(); // x1D - Offhand Swap fix
 
     @Override
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        Bukkit.getServer().getPluginManager().registerEvents(new eJoinLeaveEvents(), this);
-        Bukkit.getServer().getPluginManager().registerEvents(new eOffhandCheck(), this);
 
         for (Player player : Bukkit.getOnlinePlayers()) {
-            eOffhandCheck.lastChange.put(player, System.currentTimeMillis());
-            eOffhandCheck.warnings.put(player, 0);
+            lastChange.put(player, System.currentTimeMillis());
+            warnings.put(player, 0);
         }
     }
 
@@ -42,6 +42,8 @@ public final class WaitingServerUtilities extends JavaPlugin implements Listener
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.setJoinMessage(null);
+        lastChange.put(event.getPlayer(), System.currentTimeMillis()); // x1D - Offhand Swap fix
+        warnings.put(event.getPlayer(), 0); // x1D - Offhand Swap fix
         event.getPlayer().teleport(new Location(event.getPlayer().getWorld(), 0.5, 3.5, 0.5));
         event.getPlayer().sendMessage(ChatColor.GOLD + "Welcome to DESTROYMC.NET");
         if (event.getPlayer().hasPermission("mapcha.bypass")) {
@@ -107,5 +109,18 @@ public final class WaitingServerUtilities extends JavaPlugin implements Listener
                 }.runTaskLater(this, 1L);
             }
         }
+    }
+    // x1D - Offhand Swap fix
+    @EventHandler
+    public void onMainHandChange(PlayerSwapHandItemsEvent event) {
+        if (lastChange.get(event.getPlayer()) != null && lastChange.get(event.getPlayer()) + 250 > System.currentTimeMillis()) {
+            warnings.put(event.getPlayer(), warnings.get(event.getPlayer()) + 1);
+            event.getPlayer().sendMessage(ChatColor.GOLD + "Please slow down or you will be kicked. (" + warnings.get(event.getPlayer()) + "/5)");
+            if (warnings.get(event.getPlayer()) > 4) {
+                event.getPlayer().kickPlayer("§4§lanti riga exploit\n§7by x1D");
+                warnings.put(event.getPlayer(), 0);
+            }
+        }
+        lastChange.put(event.getPlayer(), System.currentTimeMillis());
     }
 }
